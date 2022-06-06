d995c0b0bddfd4bade14bea9d0c54963094fd753
AzizSaidrakhmonov <saidrakhmonovaziz@gmail.com>
2022-06-06 21:45:17 +0500

    FoodyApp

---
+++ src/home-section/HomeSectionAll.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import HomeSectionFood from './HomeSectionFood';
+import '../syling/homeSectionAll.css'
+
+const HomeSectionAll = () => {
+    return (
+        <div className='home-section__container'>
+            <HomeSectionFood />
+        </div>
+    );
+};
+
+
+export default HomeSectionAll;
